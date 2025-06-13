557c5db79e44b2bf5a40dc2f11900807882153b9
Vov4iK44 <vova.nechaev.spb@gmail.com>
2025-06-13 10:14:59 +0300

    added css and js example files

---
+++ main.js
@@ -0,0 +1,18 @@
+let p = "abcdefg"
+
+console.log("Hello" + "5")
+
+console.log(p)
+
+p = undefined
+console.log(p)
+
+ let Family = {
+  Mum: '52',
+  Dad: true,
+  Granny: true,
+  Son: this.Mum == '52',
+
+}
+
+console.log(Family["Son"])
